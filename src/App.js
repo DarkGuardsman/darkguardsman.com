3680ffa977dc8997904c015932aa975ed98dab21
Robert WC Seifert <rseifert.phone@gmail.com>
2019-10-07 12:52:44 -0400

    Turning site into a react app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hey this is a quick test of github pages working with github actions.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/DarkGuardsman/react-github-action-test"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
